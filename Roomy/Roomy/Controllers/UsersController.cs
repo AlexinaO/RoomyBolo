12f16ffb93dca46647946faf54f17fbc9764271b
Vincent <vleclerc@inow.fr>
2018-07-31 10:12:33 +0200

    init

---
+++ Roomy/Roomy/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            
+            /*List<string> liste = new List<string> { "toto", "titi", "tata" };
+            liste.FindAll(delegate (string s)
+            {
+                return s.Contains("a");
+            });
+
+            liste.FindAll(s => s.Contains("a"));*/
+
+
+
+
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(string email)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+
+        [Display(Name = "email", ResourceType = typeof(Resources.ResourceRoomy))]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmedPassword { get; set; }
+        public DateTime BithDate { get; set; }       
+    }
+}
